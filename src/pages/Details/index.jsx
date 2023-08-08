1d18256b34f1d494fcc9edfe2fecec41d1f1d71c
Rodrigo <rodrigopretes64@gmail.com>
2023-08-08 12:51:57 -0300

    primeiro commit - inicializando o react

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./styles.css";
+
+export default function Details(){
+
+  return(
+    <Fragment>
+    <h1>Hello World!</h1>
+    <span>rodrigo pretes</span>
+    </Fragment>
+  );
+}
